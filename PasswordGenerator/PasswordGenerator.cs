3f2289070cee67284639c31995183d00e11e484f
KYLE BLACKWELL <102472889@student.swin.edu.au>
2019-08-21 10:15:54 +1000

    Added PasswordGen Code

--- PasswordGenerator/PasswordGenerator.cs
+++ PasswordGenerator/PasswordGenerator.cs
@@ -1,19 +1,62 @@
 ﻿using System;
+using System.Linq;
 
 namespace PasswordGenerator
 {
-        public class Generator {
+    public class Generator
+    {
 
-        public string PasswordGenOne(string firstname, string lastname, int age) {
-            throw new NotImplementedException();
+        public string PasswordGenOne(string firstname, string lastname, int age)
+        {
+
+
+            firstname.ToLower();
+            string lName = lastname.ToUpper();
+            char[] lArray = lName.ToCharArray();
+
+
+            int year = 2019 - age;
+
+            return firstname.ToLower() + lArray[0] + "_" + year;
+
+
+
+            //throw new NotImplementedException();
         }
 
-        public string PasswordGenTwo(string firstname, string lastname, int age) {
-            throw new NotImplementedException();
+        public string PasswordGenTwo(string firstname, string lastname, int age)
+        {
+
+            int year = 2019 - age;
+
+            string fName = firstname.ToUpper();
+            char[] fArray = fName.ToCharArray();
+
+            return lastname + year + fArray[0];
+
+            //throw new NotImplementedException();
         }
 
-        public string PasswordGenThree(int length) {
-            throw new NotImplementedException();
+        public string PasswordGenThree(int length)
+        {
+
+
+            string input = "AB@#ef12";
+
+            if (input.Any(c => !char.IsLetterOrDigit(c)) && input.Any(char.IsUpper))
+            {
+                return input.Length.ToString();
+            }
+            else
+            {
+                throw new NotImplementedException();
+
+            }
+
+
+
+
+
         }
     }
 }
